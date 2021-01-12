85020f7d653fd03835f6acb3625aa80efc8f4459
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-01-12 15:53:07 +0500

    init project created

---
+++ src/iCook/iCook/iCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCookApp.swift
+//  iCook
+//
+//  Created by Bohirjon Akhmedov on 12/01/21.
+//
+
+import SwiftUI
+
+@main
+struct iCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
