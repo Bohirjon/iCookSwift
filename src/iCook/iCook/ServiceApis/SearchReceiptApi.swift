211a2164dc101e8325c2f83838f10ba09a374fb0
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-01-12 16:49:06 +0500

    search receipts query implemented

--- src/iCook/iCook/ServiceApis/SearchReceiptApi.swift
+++ src/iCook/iCook/ServiceApis/SearchReceiptApi.swift
@@ -15,17 +15,35 @@ protocol SearchReceiptApiDelegate {
 
 protocol SearchReceiptApiProtocol {
     var searchReceiptApiDelegate: SearchReceiptApiDelegate! { get }
-    func search(queryKey: String)
+    func search(queryKey: String, number: Int)
 }
 
 class SearchReceiptApi: SearchReceiptApiProtocol {
     let searchReceiptApiDelegate: SearchReceiptApiDelegate!
-   
+    
     init(searchReceiptApiDelegate: SearchReceiptApiDelegate) {
         self.searchReceiptApiDelegate = searchReceiptApiDelegate
     }
     
-    func search(queryKey: String) {
-        
+    func search(queryKey: String, number: Int) {
+        let urlString = "\(AppConstract.ReceiptServiceEndPoint)?query=\(queryKey)&number=\(number.description)"
+        if let url = URL(string: urlString) {
+            
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let safeData = data  {
+                    let jsonDecoder = JSONDecoder()
+                    do {
+                        let searchReceiptResult  = try jsonDecoder.decode(SearchReceiptResult.self, from: safeData)
+                        self.searchReceiptApiDelegate?.onSearchFinish(searchReceiptResult: searchReceiptResult)
+                    } catch let error {
+                        self.searchReceiptApiDelegate?.onSearchFailed(error: error)
+                    }
+                }
+            }.resume()
+            
+        } else {
+            print("bad url: = \(urlString)")
+            self.searchReceiptApiDelegate?.onSearchFailed(error: URLError.init(.badURL))
+        }
     }
 }
