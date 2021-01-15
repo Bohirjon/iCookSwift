a6e9ba8e5b90197a69e6f0672e2a88a03e5c0d82
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-01-15 10:38:19 +0500

    Created model

--- src/iCook/iCook/Views/ReceiptDetailView.swift
+++ src/iCook/iCook/Views/ReceiptDetailView.swift
@@ -6,8 +6,13 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ReceiptDetailView: View {
+    private let receipt: Receipt
+    init(receiptId: Int) {
+        
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
