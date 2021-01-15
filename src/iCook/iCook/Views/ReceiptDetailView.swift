7b8bf98afea042986674537d80d566877e921372
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-01-15 10:02:55 +0500

    view created

---
+++ src/iCook/iCook/Views/ReceiptDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ReceiptDetailView.swift
+//  iCook
+//
+//  Created by Bohirjon Akhmedov on 15/01/21.
+//
+
+import SwiftUI
+
+struct ReceiptDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReceiptDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReceiptDetailView()
+    }
+}
